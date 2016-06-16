3bc15d5336e619592f0e38511340f353b777ea0f
Brian Jimerson <bjimerson@pivotal.io>
2016-06-16 11:13:26 -0500

    Initial commit

---
+++ src/main/java/io/pivotal/cf/workshop/CFWorkshopBootApplication.java
@@ -0,0 +1,21 @@
+package io.pivotal.cf.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main entry point for the application.
+ * @author Brian Jimerson
+ *
+ */
+@SpringBootApplication
+public class CFWorkshopBootApplication {
+
+	/**
+	 * The main entry point for the application.
+	 * @param args Any command line args passed.
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(CFWorkshopBootApplication.class, args);
+	}
+}
