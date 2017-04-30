e42027865ca5ea480a4dce9e0236a94ad5db7fda
Brian Jimerson <bjimerson@pivotal.io>
2017-04-30 13:39:38 -0400

    Add eureka client

--- src/main/java/io/pivotal/workshop/CfWorkshopDemoApplication.java
+++ src/main/java/io/pivotal/workshop/CfWorkshopDemoApplication.java
@@ -2,6 +2,7 @@ package io.pivotal.workshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Main entry point for the application.
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CfWorkshopDemoApplication {
 
 	/**
